ec4e885b14b7a6a92be3c53d604225b15acdc2d3
Daniel Norris <daniel.norris@acquia.com>
2013-12-06 10:50:59 -0500

    Add Vagrantfile, puppet manifest

---
+++ manifests/main.pp
@@ -0,0 +1,11 @@
+exec {'apt-update':
+    command => '/usr/bin/apt-get update',
+}
+
+Exec['apt-update'] -> Package <| |>
+
+$essential_packages = ['git', 'build-essential']
+
+package {$essential_packages:
+    ensure => latest,
+}
